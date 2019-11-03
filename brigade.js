d2d8f060571a1ee2878724e6ec3b98c89aa4b917
Jakob Ehn <jakob@ehn.nu>
2019-11-03 15:02:21 +0100

    sd

--- brigade.js
+++ brigade.js
@@ -1,6 +1,6 @@
 const { events, Job, Group } = require("brigadier");
 
-events.on("push", async () => {
+events.on("push", async (e, p) => {
     var compileStep = new Job("compile", "mcr.microsoft.com/dotnet/core/sdk:3.0")
     compileStep.tasks = [
         "cd src",
@@ -23,7 +23,7 @@ events.on("push", async () => {
   
     var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
     slack.env = {
-      SLACK_WEBHOOK: "p.secrets.SLACK_WEBHOOK",
+      SLACK_WEBHOOK: p.secrets.SLACK_WEBHOOK,
       SLACK_USERNAME: "Brigade",
       SLACK_TITLE: "Hello from Brigade",
       SLACK_MESSAGE: "This is a message from Brigade"
